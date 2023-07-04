48731cefadcddb3a5ade5a987f646a1f60fc99e7
ZhechoZhechev <mishomishev024@gmail.com>
2023-07-04 17:06:59 +0300

    DoctorSpeciality entity configuration added

---
+++ SimpleClinic.Infrastructure/Configuration/DoctorSpecialityEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleClinic.Infrastructure.Configuration;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SimpleClinic.Infrastructure.Entities;
+
+/// <summary>
+/// DoctorSpeciality entity configuration
+/// </summary>
+public class DoctorSpecialityEntityConfiguration : IEntityTypeConfiguration<DoctorSpeciality>
+{
+    /// <summary>
+    /// Configure method from IEntityTypeConfiguration interface
+    /// </summary>
+    /// <param name="builder">builder class</param>
+    public void Configure(EntityTypeBuilder<DoctorSpeciality> builder)
+    {
+        builder
+            .HasKey(pk => new {pk.DoctorId, pk.SpecialityId});
+    }
+}
--- SimpleClinic.Infrastructure/SimpleClinicDbContext.cs
+++ SimpleClinic.Infrastructure/SimpleClinicDbContext.cs
@@ -19,6 +19,22 @@ public class SimpleClinicDbContext : IdentityDbContext<ApplicationUser>
     {
     }
 
+    public DbSet<Doctor> Doctors { get; set; } = null!;
+    public DbSet<DoctorAppointment> DoctorAppointments { get; set; } = null!;
+    public DbSet<DoctorSpeciality> DoctorsSpecialities { get; set; } = null!;
+    public DbSet<MedicalHistory> MedicalHistories { get; set; } = null!;
+    public DbSet<Medicament> Medicaments { get; set; } = null!;
+    public DbSet<NextOfKin> NextOfKins { get; set; } = null!;
+    public DbSet<Patient> Patients { get; set; } = null!;
+    public DbSet<Prescription> Prescriptions { get; set; } = null!;
+    public DbSet<Schedule> Schedules { get; set; } = null!;
+    public DbSet<Service> Services { get; set; } = null!;
+    public DbSet<ServiceAppointment> ServiceAppointments { get; set; } = null!;
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; } = null!;
+    public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; } = null!;
+    public DbSet<Speciality> Specialities { get; set; } = null!;
+    public DbSet<TimeSlot> TimeSlots { get; set; } = null!;
+
     /// <summary>
     /// Method for modelating entities
     /// </summary>
