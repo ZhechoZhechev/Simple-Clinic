74e857b410ee73f1d58a7ea79b9487a88fc406bc
ZhechoZhechev <mishomishev024@gmail.com>
2023-07-27 16:54:34 +0300

    All services for patients views added.

---
+++ SimpleClinic/Areas/Patient/Controllers/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleClinic.Areas.Patient.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SimpleClinic.Core.Contracts;
+using SimpleClinic.Core.Models.PatientModels;
+
+public class ServiceController : Controller
+{
+    private readonly IServiceService serviceService;
+
+    public ServiceController(IServiceService serviceService)
+    {
+        this.serviceService = serviceService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> All([FromQuery] AllServicesPaginationModel queryModel)
+    {
+        var queryResult = await serviceService.All(queryModel.CurrentPage, queryModel.ServicesPerPage);
+
+        queryModel.TotalServicesCount = queryResult.TotalServicesCount;
+        queryModel.Services = queryResult.Services;
+
+        return View(queryModel);
+    }
+
+    }
