66a687ccb53cacbe86d1760d43a5bf65ec0e26b6
ZhechoZhechev <mishomishev024@gmail.com>
2023-07-28 14:53:55 +0300

    Doctors area home views and controllers added.

---
+++ SimpleClinic/Areas/Doctor/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleClinic.Areas.Doctor.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using SimpleClinic.Common;
+
+[Authorize(Roles = RoleNames.DoctorRoleName)]
+[Area("Doctor")]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
