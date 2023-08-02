733b40343c6cdca2f217e8f8394113887fba3663
ZhechoZhechev <mishomishev024@gmail.com>
2023-08-02 17:16:16 +0300

    Layout datepicker script added.

--- SimpleClinic/Areas/Patient/Controllers/AppointmentController.cs
+++ SimpleClinic/Areas/Patient/Controllers/AppointmentController.cs
@@ -1,7 +1,12 @@
 ﻿namespace SimpleClinic.Areas.Patient.Controllers;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+using SimpleClinic.Common;
+
+[Authorize(Roles = RoleNames.PatientRoleName)]
+[Area("Patient")]
 public class AppointmentController : Controller
 {
     public IActionResult ChooseDate(string id)
