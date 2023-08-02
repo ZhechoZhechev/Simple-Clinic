a5b79a2efb8411dfea5659284b4829342d7bd9ed
ZhechoZhechev <mishomishev024@gmail.com>
2023-08-02 15:33:16 +0300

    Several changes.

---
+++ SimpleClinic/Areas/Patient/Controllers/AppointmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleClinic.Areas.Patient.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult ChooseDate(string id)
+        {
+            ViewBag.DoctorId = id;
+
+            return View();
+        }
+    }
+}
